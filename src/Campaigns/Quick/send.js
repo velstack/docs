3445072e9f73d69e8f6406fa656b3dd0ca666cbc
sammyfort <thesamuelfort@gmail.com>
2023-08-19 17:41:09 -0400

    Javascript

---
+++ src/Campaigns/Quick/send.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const data = {
+    title: "Announcement",
+    sender: "VELSTACK",
+    recipient: "0205550368",
+    message: 'First API message from Velstack'
+}
+
+ axios.post('https://sms.velstack.com/api/messaging/quick/sms', data, {
+    headers: {
+        Authorization: "Bearer " + "API_KEY",
+        Accept: "application/json",
+    }
+  }).then((response) => {
+       alert('Message sent successfully')
+
+  }).catch((errors) => {
+      alert('Something went wrong')
+  })
